204e60e1af029cfed98eff38321b47e3a6a78302
mariano garcia <mariano812811@gmail.com>
2023-08-18 20:37:12 -0300

    avances en carpetas components

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+const ItemListContainer = (props) => {
+  return (
+    <main>
+      <h1> {props.greeting} </h1>
+    </main>
+  );
+};
+
+export default ItemListContainer;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Ecommerce</h1>
+      <section>
+        <button>Celuares</button>
+        <button>Tablets</button>
+        <button>NoteBooks</button>
+      </section>
+    </nav>
+  );
+};
+
+export default Navbar;
